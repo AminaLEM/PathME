d714a69734718346824e53f7699bb0c044fbf74b
Amina LEMSARA <45292279+AminaLEM@users.noreply.github.com>
2019-12-08 09:40:13 +0100

    Update FeatureSelection_Variance.R

--- Competitive methods/FeatureSelection_Variance.R
+++ Competitive methods/FeatureSelection_Variance.R
@@ -1,6 +1,6 @@
-\\ features selection based on the variance (sorting features in decreasing order by variance and then picking the top k ones, k may be diffrent across datasets)
+# features selection based on the variance (sorting features in decreasing order by variance and then picking the top k ones, k may be diffrent across datasets)
 k = 200 // Number of features to select
-\\ Gene expression dataset
+# Gene expression dataset
 dat= inputge
 matr= rep(0,dim(dat)[2])
 for (i in 1:dim(dat)[2])
@@ -11,7 +11,7 @@ for (i in 1:dim(dat)[2])
 ind= tail(order(matr),k)
 X1= dat[,ind]
 
-\\ CNV dataset
+# CNV dataset
 dat= inputcnv
 matr= rep(0,dim(dat)[2])
 for (i in 1:dim(dat)[2])
@@ -22,7 +22,7 @@ for (i in 1:dim(dat)[2])
 ind= tail(order(matr),k)
 X2= dat[,ind]
 
-\\ miRNA dataset
+# miRNA dataset
 dat= inputmiRNA
 matr= rep(0,dim(dat)[2])
 for (i in 1:dim(dat)[2])
